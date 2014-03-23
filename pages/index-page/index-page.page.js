ede94c7a7b75a0cf6b0509c8a88c35738a46026e
Vadim Makishvili <makishvili@yandex-team.ru>
2014-03-23 16:20:06 +0200

    Категории рефакторинг

--- pages/index-page/index-page.page.js
+++ pages/index-page/index-page.page.js
@@ -6,17 +6,20 @@ module.exports = function (pages) {
         var requestedPostPath = params.path;
         var allPosts = params.data.posts;
 
-        // Отфильтровать статьи по категории
-        allPosts.filterPostsByCategory(requestedPostPath);
-
         // Количество страниц с учётом PAGELENGTH
         var pageCount = allPosts.getPageCount(PAGELENGTH);
 
         // Номер текущей страницы
         var pageNumber = parseInt(requestedPostPath.split('/').pop()) || 1;
 
+        var requestedCategory;
+        if (params.path.indexOf('category/') !== -1) {
+            // получить значение категории из урла
+            requestedCategory = requestedPostPath.replace('category/', '');
+        }
+
         // Посты для текущей страницы
-        var posts = allPosts.selectPostsForPage(pageNumber, PAGELENGTH);
+        var posts = allPosts.selectPostsForPage(pageNumber, PAGELENGTH, requestedCategory);
 
         // Номера для предыдущей и следующей старницы
         // null чтобы для этих случаев не генерились кнопки
